95455c1a115605661f8a4d58a5b8494575610025
Hrdayesh Patel <hpatel@securitycompass.com>
2018-10-04 15:54:26 -0400

    - Abstract out URL - Update echo statements - Add timeout to `curl`

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-until curl --output /dev/null --silent --head --fail http://"${CERT_DOMAIN}"; do
-    echo "Cannot resolve ${CERT_DOMAIN}..."
+echo "Starting 'certbot' entrypoint..."
+
+cert_url="http://${CERT_DOMAIN}"
+until curl --output /dev/null --silent --head --fail --connect-timeout 30 "${cert_url}"; do
+    echo "Cannot resolve '${cert_url}, sleeping and trying again..."
     sleep 60
 done
 
-echo "Response from http://${CERT_DOMAIN}, attempting to obtain certificate..."
-
+echo "Successful response from '${cert_url}', attempting to obtain certificate..."
 /usr/local/bin/run_certbot.sh -e "${ADMIN_EMAIL}" -d "${CERT_DOMAIN}" -r /challenges
