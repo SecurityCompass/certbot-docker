e96614ed40ff2831efe8db92a1ae69d1c15d3db4
Hrdayesh Patel <hpatel@securitycompass.com>
2018-10-05 15:20:29 -0400

    Add missing quote

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,7 +4,7 @@ echo "Starting 'certbot' entrypoint..."
 
 cert_url="http://${CERT_DOMAIN}"
 until curl --output /dev/null --silent --head --fail --connect-timeout 30 "${cert_url}"; do
-    echo "Cannot resolve '${cert_url}, sleeping and trying again..."
+    echo "Cannot resolve '${cert_url}', sleeping and trying again..."
     sleep 60
 done
 
